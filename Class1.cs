4b6f3272802f298f16c71d5083acb8891ead084c
Nanda-Nandan <nandannanda01@gmail.com>
2017-10-06 18:37:07 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Player
+    {
+        private List<int> SquarePosition
+        {
+            get;set;
+        }
+        public Player(string input) {
+            this.Initialize(input);
+        }
+        public void Initialize(string inputstring)
+        {
+            SquarePosition= inputstring.Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+        .Select(item => int.Parse(item))
+        .ToList();
+        }
+    }
+}
